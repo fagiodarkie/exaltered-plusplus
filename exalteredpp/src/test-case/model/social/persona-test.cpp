04d1647400f792fa2357cf5c346ff0b08866f785
fagiodarkie <fagio.darkie@gmail.com>
2019-07-09 16:36:33 +0200

    cover last edge cases of persona

--- exalteredpp/src/test-case/model/social/persona-test.cpp
+++ exalteredpp/src/test-case/model/social/persona-test.cpp
@@ -81,4 +81,50 @@ TEST_CASE("persona")
     sut.decrease_philosophy("p1");
     REQUIRE(sut.philosophies().isEmpty());
   }
+
+  SECTION("should break on attempts to handle non existing philosophies")
+  {
+    character::social::persona sut(20, 7, 6, 5, 4, 3, 2, 4);
+    REQUIRE_NOTHROW(sut.add_philosophy(character::social::philosophy("p1", 1)));
+    REQUIRE_THROWS(sut.increase_philosophy("p2"));
+    REQUIRE_THROWS(sut.decrease_philosophy("p2"));
+  }
+
+  SECTION("should set persona values correctly")
+  {
+    character::social::persona sut(20, 7, 6, 5, 4, 3, 2, 4);
+    sut.set_persona              (8);
+    sut.set_compulsions_specific (8);
+    sut.set_emotions_specific    (8);
+    sut.set_illusions_specific   (8);
+    sut.set_motivations_specific (8);
+    sut.set_serfdom_specific     (8);
+    sut.set_max_philosophies     (8);
+    sut.set_max_philosophy_value (8);
+
+    REQUIRE(sut.get_persona              () == 8);
+    REQUIRE(sut.get_compulsions_specific () == 8);
+    REQUIRE(sut.get_emotions_specific    () == 8);
+    REQUIRE(sut.get_illusions_specific   () == 8);
+    REQUIRE(sut.get_motivations_specific () == 8);
+    REQUIRE(sut.get_serfdom_specific     () == 8);
+    REQUIRE(sut.get_max_philosophies     () == 8);
+    REQUIRE(sut.get_max_philosophy_value () == 8);
+  }
+
+  SECTION("should get all emotions with relevant values")
+  {
+    character::social::persona sut(20, 7, 6, 5, 4, 3, 2, 4);
+    for (auto emotion: character::social::BASE_EMOTIONS)
+      {
+        sut.set_base_emotion_bonus(emotion, 2);
+      }
+
+    for (auto emotion: character::social::BASE_EMOTIONS)
+      {
+        REQUIRE(sut.get_emotion_bonus_for(emotion) == 2);
+        REQUIRE(sut.get_emotion_bonus_for(character::social::MIDDLE_EMOTION_GRADES[emotion]) == 4);
+        REQUIRE(sut.get_emotion_bonus_for(character::social::INTIMATE_EMOTION_GRADES[emotion]) == 6);
+      }
+  }
 }
