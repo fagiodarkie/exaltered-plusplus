07199faebc869f8bae543a77f69db0cd72fcfaf9
Jacopo Freddi <fagio.darkie@gmail.com>
2019-08-25 15:35:55 +0200

    fix tests

--- exalteredpp/src/test-case/managers/character_manager-test.cpp
+++ exalteredpp/src/test-case/managers/character_manager-test.cpp
@@ -3,6 +3,7 @@
 #include "errors/characternotfoundexception.h"
 #include "managers/character_manager.h"
 #include "db_abstraction_mock.h"
+#include "calculator/worker/common_worker.h"
 #include "qt-test/quick_chargen.h"
 
 TEST_CASE("character_manager")
@@ -36,7 +37,9 @@ TEST_CASE("character_manager")
   SECTION("should create a new character if requested")
   {
     try {
-      auto generated = sut.create_character("name", character::creation::TYPE_MORTAL_HERO, character::exalt::caste::NO_CASTE, character::attributes(), character::abilities(), character::virtues::virtues(), character::power::power_container());
+      auto attributes = character::attributes();
+      attributes[character::attribute_names::CONSTITUTION].set_value(2);
+      auto generated = sut.create_character(calculator::worker::human_worker(), "name", character::creation::TYPE_MORTAL_HERO, character::exalt::caste::NO_CASTE, attributes, character::abilities(), character::virtues::virtues(), character::power::power_container());
       REQUIRE(generated);
     }
     catch(...)
